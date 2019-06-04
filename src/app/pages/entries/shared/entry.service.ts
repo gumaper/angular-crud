efb1208f936b5fc7be756ef4dfbcdf3614cc457d
gumaper <gumaper@outlook.com>
2019-06-04 19:55:28 -0400

    improving EntryService with BaseResourceService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,8 +1,10 @@
-import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { Injectable, Injector } from '@angular/core';
 
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { CategoryService } from './../../categories/shared/category.service';
 import { Entry } from './entry.model';
+import { flatMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,44 @@ export class EntryService extends BaseResourceService<Entry> {
   constructor(protected injector: Injector, private categoryService: CategoryService) {
     super('api/entries', injector);
    }
+
+  create(entry: Entry): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return super.create(entry);
+      })
+    );
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+
+        return super.update(entry);
+      })
+    );
+  }
+
+
+
+  // PRIVATE METHODS
+
+  protected jsonDataToResources(jsonData: any[]): Entry[] {
+    const entries: Entry[] = [];
+
+    jsonData.forEach(element => {
+      const entry = Object.assign(new Entry(), element);
+      entries.push(entry);
+    });
+
+    return entries;
+  }
+
+  protected jsonDataToResource(jsonData: any): Entry {
+    return Object.assign(new Entry(), jsonData);
+  }
+
 }
