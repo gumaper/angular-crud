402a13365d4e4625dff5a874be37912a8394d9e1
gumaper <gumaper@outlook.com>
2019-06-05 11:12:34 -0400

    improving CategoryList with BaseResourceList

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component';
 
 import { CategoryService } from './../shared/category.service';
 import { Category } from '../shared/category.model';
@@ -8,27 +10,9 @@ import { Category } from '../shared/category.model';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  categories: Category[] = [];
-
-  constructor(private categoryService: CategoryService) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao carregar a lista')
-    );
-  }
-
-  deleteCategory(category) {
-    const mustDelete = confirm('Desejar realmente excluir esse item?');
-
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element !== category),
-        () => alert('Error ao excluir')
-      );
-    }
-  }
+  constructor(private categoryService: CategoryService) {
+    super(categoryService);
+   }
 }
